ab1a0da6120e903283fee14abee173d0828d3291
Jeff <jmills@delphicdigital.com>
2016-08-25 14:44:42 -0400

    Started more

--- skeletor.flyouts.js
+++ skeletor.flyouts.js
@@ -12,12 +12,22 @@ define(['jquery', 'skeletor.core'],function ($, Skeletor){
 	Flyouts.VERSION = '0.0.1';
 
 	Flyouts.DEFAULTS = {
-		cssClass:'flyouts'
+		uuidPrefix: "accessible-megamenu", // unique ID's are required to indicate aria-owns, aria-controls and aria-labelledby
+		menuClass: "accessible-megamenu", // default css class used to define the megamenu styling
+		topNavItemClass: "accessible-megamenu-top-nav-item", // default css class for a top-level navigation item in the megamenu
+		panelClass: "accessible-megamenu-panel", // default css class for a megamenu panel
+		panelGroupClass: "accessible-megamenu-panel-group", // default css class for a group of items within a megamenu panel
+		hoverClass: "hover", // default css class for the hover state
+		focusClass: "focus", // default css class for the focus state
+		openClass: "open" // default css class for the open state
 	};
 
 	Skeletor.Plugin.create(Flyouts, {
 		_init: function(element) {
 			console.log('init flyouts')
+		},
+		_test: function(element) {
+			console.log('init test')
 		},
 		open: function(){
 			console.log('open flyout')
